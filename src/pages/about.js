fb22d7c904c82c23046e60ae30116765137d457f
nusaK <hkknalaka@gmail.com>
2022-09-23 20:24:39 +0530

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import * as  React from 'react';
+
+export default function AboutPage(){
+  return(
+    <main>
+      <h1>Hello about Gatsby</h1>
+    </main>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as  React from 'react';
+
+export default function IndexPage(){
+  return(
+    <main>
+      <h1>Hello World Gatsby</h1>
+    </main>
+  );
+}
+
+
