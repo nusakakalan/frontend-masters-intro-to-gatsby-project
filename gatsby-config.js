12a2f4a64734261dd20019e8f83ccc7e7c373315
nusaK <hkknalaka@gmail.com>
2022-09-26 02:24:45 +0530

    fix errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.com',
-    title: 'Frontend masters Intro To Gatsby',
-    description: 'Frontend masters Intro to Gatsby course project',
-    image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Frontend Masters Intro to Gatsby',
+    description: 'Frontend Masters Intro to Gatsby course projects',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    //this set of plugins is to enable creating MDX blog posts from the src/posts folder
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
-        path: `${__dirname}/src/posts/`,
+        path: `${__dirname}/src/posts`,
       },
     },
     {
       resolve: 'gatsby-plugin-page-creator',
       options: {
-        path: `${__dirname}/src/posts/`,
+        path: `${__dirname}/src/posts`,
       },
     },
     'gatsby-remark-images',
@@ -26,37 +26,34 @@ module.exports = {
       resolve: 'gatsby-plugin-mdx',
       options: {
         gatsbyRemarkPlugins: [
-                     {
-                      resolve: 'gatsby-remark-images',
-               options: {
-                maxWidth: 1200,
-                 },
-           },
-         ],
-
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
         defaultLayouts: {
           posts: require.resolve('./src/components/post-layout.js'),
-
         },
       },
     },
-    //end of MDX configurations
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/images/`,
+        path: `${__dirname}/src/images`,
       },
     },
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
-           resolve: 'gatsby-source-sanity',
-        options: {
-           projectId: 'vnkupgyb',
-             dataset: 'production',
-           },
-     },
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'vnkupgyb',
+        dataset: 'production',
+      },
+    },
   ],
 };
--- src/pages/episode/{SanityEpisode.slug__current}.js
+++ src/pages/episode/{SanityEpisode.slug__current}.js
@@ -27,10 +27,11 @@ export default function SanityEpisode({ data }) {
 
   return (
     <Layout title={episode.title} description={episode.description}>
-      <GatsbyImage
+      {episode.image?.asset?.gatsbyImageData &&
+     (<GatsbyImage
         image={episode.image.asset.gatsbyImageData}
         alt={episode.title}
-      />
+      />)}
       <h1>{episode.title}</h1>
       <p>
         (posted {episode.date}) — {episode.description}
